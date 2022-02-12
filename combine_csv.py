f2d3a3e3b090001436fe2a12440df3914927f679
Rachel Nguyen <baongoc21099@ku.edu>
2022-02-13 01:41:58 +0700

    initialize commit

---
+++ combine_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os 
+import sys
+
+
+def combined_csv(list_of_files_path, name_of_combine_file):
+    initial_csv = list_of_files_path[0]
+    initial_df = pd.read_csv(initial_csv, dtype='object')
+    initial_df['filename'] = [list_of_files_path[0] for i in range(len(initial_df))]
+    for i in list_of_files_path[1:]:
+        new_csv = i
+        new_df = pd.read_csv(new_csv, dtype='object')
+        new_df['filename'] = [i for j in range(len(new_df))]
+        initial_df = pd.concat([initial_df, new_df], axis=0)
+    initial_df['category'] = initial_df.category.apply(lambda x: x.replace("\\", ''))
+    initial_df['category'] = initial_df.category.apply(lambda x: x.replace('"', ''))
+    initial_df.to_csv(name_of_combine_file)
+
+if __name__ == "__main__":
+    list_of_files = sys.argv[:len(sys.argv)-2]
+    assert [os.path.exists(i) for i in list_of_files], "Invalid Files"
+    name_of_com_file = sys.argv[-1]
+    assert sys.argv[-2] != "\>", "Invalid Expression!"
+    combined_csv(list_of_files, name_of_com_file)
+        
+    
---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+install_requires = \
+["pandas==1.3.5"
+]
+
+setup_kwargs = {
+    'name': 'combine_csv',
+    'version': '0.0.1',
+    'description': 'A program to combine csv',
+    'py_modules': ['combine_csv'],
+    'entry_points':{
+        'console_scripts':
+        ['combine_csv=combine_csv:main']
+    },
+    'long_description': long_description,
+    'author': 'Rachel Nguyen',
+    'author_email': 'baongoc21099@ku.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+setup(**setup_kwargs)
